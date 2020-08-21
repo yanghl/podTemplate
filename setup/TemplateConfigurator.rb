d332cc77f4732e4ee499cc3beaf940d4f7f930b7
杨红磊 <yanghl01@beantechs.cn>
2020-08-21 14:58:19 +0800

    delete test target

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -163,11 +163,11 @@ module Pod
     end
 
     def set_test_framework(test_type, extension, folder)
-      content_path = "setup/test_examples/" + test_type + "." + extension
-      tests_path = "templates/" + folder + "/Example/Tests/Tests." + extension
-      tests = File.read tests_path
-      tests.gsub!("${TEST_EXAMPLE}", File.read(content_path) )
-      File.open(tests_path, "w") { |file| file.puts tests }
+      # content_path = "setup/test_examples/" + test_type + "." + extension
+      # tests_path = "templates/" + folder + "/Example/Tests/Tests." + extension
+      # tests = File.read tests_path
+      # tests.gsub!("${TEST_EXAMPLE}", File.read(content_path) )
+      # File.open(tests_path, "w") { |file| file.puts tests }
     end
 
     def rename_template_files
